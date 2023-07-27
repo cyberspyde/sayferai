e24e7e833632219765aeea9f6cd1e6ccfc240dbd
cyberspyde <ilhomkobilovspecial@gmail.com>
2023-07-27 22:05:11 +0900

    'update'

--- utils/automation/emergency.py
+++
@@ -1,9 +0,0 @@
-def activate_emergency():
-    global emergency_active
-    emergency_active = True
-    print("Emergency activated")
-
-def deactivate_emergency():
-    global emergency_active
-    emergency_active = False
-    print("Emergency deactivated")
